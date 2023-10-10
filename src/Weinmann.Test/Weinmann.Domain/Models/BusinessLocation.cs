fde3048aa928a6cafee13470901d091b0bf22203
Andrii Panchyshyn <panchyshyn.andrew@gmail.com>
2023-10-10 14:45:08 +0300

    #2 Create domain models

---
+++ src/Weinmann.Test/Weinmann.Domain/Models/BusinessLocation.cs
@@ -0,0 +1,9 @@
+﻿namespace Weinmann.Domain.Models;
+
+internal class BusinessLocation
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string TelephoneNumber { get; set; }
+}
---
+++ src/Weinmann.Test/Weinmann.Domain/Models/Customer.cs
@@ -0,0 +1,7 @@
+﻿namespace Weinmann.Domain.Models;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
