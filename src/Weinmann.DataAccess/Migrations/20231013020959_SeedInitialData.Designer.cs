88d1a60cf0feb18111d25d04c28d3ac1bfa8a242
Andrii Panchyshyn <panchyshyn.andrew@gmail.com>
2023-10-13 05:58:37 +0300

    Implement docker support

--- src/Weinmann.DataAccess/Migrations/20231013020959_SeedInitialData.Designer.cs
+++ src/Weinmann.DataAccess/Migrations/20231013020959_SeedInitialData.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Weinmann.DataAccess;
 
@@ -10,9 +11,11 @@ using Weinmann.DataAccess;
 namespace Weinmann.DataAccess.Migrations
 {
     [DbContext(typeof(WeinmannDataContext))]
-    partial class WeinmannDataContextModelSnapshot : ModelSnapshot
+    [Migration("20231013020959_SeedInitialData")]
+    partial class SeedInitialData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
